91b8a5e4e9e3211d2d2069816aa2899c5dc9bd92
vanxd <373437500@qq.com>
2016-02-27 23:37:56 +0800

    框架搭好

---
+++ src/main/java/com/xiaodongchu/service/PermissionService.java
@@ -0,0 +1,13 @@
+package com.xiaodongchu.service;
+
+import com.xiaodongchu.entity.Permission;
+
+/**
+ * <p>User: Zhang Kaitao
+ * <p>Date: 14-1-28
+ * <p>Version: 1.0
+ */
+public interface PermissionService {
+    public Permission createPermission(Permission permission);
+    public void deletePermission(Long permissionId);
+}
---
+++ src/main/java/com/xiaodongchu/service/PermissionServiceImpl.java
@@ -0,0 +1,27 @@
+package com.xiaodongchu.service;
+
+import com.xiaodongchu.dao.PermissionDao;
+import com.xiaodongchu.entity.Permission;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+/**
+ * <p>User: Zhang Kaitao
+ * <p>Date: 14-1-28
+ * <p>Version: 1.0
+ */
+@Service("permissionService")
+@Transactional
+public class PermissionServiceImpl implements PermissionService {
+    @Autowired
+    private PermissionDao permissionDao;
+
+    public Permission createPermission(Permission permission) {
+        return permissionDao.createPermission(permission);
+    }
+
+    public void deletePermission(Long permissionId) {
+        permissionDao.deletePermission(permissionId);
+    }
+}
