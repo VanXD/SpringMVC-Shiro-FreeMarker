e32567786c0b46d5e4c7f2882447be025ea7ce6b
Van Xiao Dong <373437500@qq.com>
2016-03-03 16:14:42 +0800

    freemarker + shiro 集成完毕，标签也可使用

---
+++ src/main/java/com/xiaodongchu/component/ShiroTagFreeMarkerConfigurer.java
@@ -0,0 +1,18 @@
+package com.xiaodongchu.component;
+
+import com.jagregory.shiro.freemarker.ShiroTags;
+import freemarker.template.TemplateException;
+import org.springframework.web.servlet.view.freemarker.FreeMarkerConfigurer;
+
+import java.io.IOException;
+
+/**
+ * Created by wyd on 2016/3/3.
+ */
+public class ShiroTagFreeMarkerConfigurer extends FreeMarkerConfigurer {
+    @Override
+    public void afterPropertiesSet() throws IOException, TemplateException {
+        super.afterPropertiesSet();
+        this.getConfiguration().setSharedVariable("shiro", new ShiroTags());
+    }
+}
